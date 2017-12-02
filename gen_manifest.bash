39621f750c194ca69dbdfcfb11297e03599f6031
Simao Gomes Viana <xdevs23@outlook.com>
2017-12-02 13:21:20 +0100

    gen_manifest: a bit of cleanup & add auto maintainer var assign
    Change-Id: I93546e2f103cab23b802631841eefab94c0eaee2

--- gen_manifest.bash
+++ gen_manifest.bash
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
 case $1 in
@@ -8,56 +7,62 @@ case $1 in
     echo -n "$@" > ${m_device}-maintainer.txt
   ;;
   *)
-
-version="$XOS_VERSION"
-device=$(echo $version | cut -d _ -f 2)
-android=$(echo $version | cut -d _ -f 3)
-product=${version%_*}
-product=${product%_*}
-date=$(echo $version | cut -d _ -f 3 | cut -d . -f 1)
-product=${product/XOS_/}
-
-function write_xml() {
-  echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
-  echo "<ROM>"
-  echo "  <RomName>$product</RomName>"
-  echo "  <VersionName><![CDATA[ $version ]]></VersionName>"
-  echo "  <VersionNumber type=\"integer\">"$(date +%Y%m%d)"</VersionNumber>"
-  echo "  <DirectUrl>https://sourceforge.net/projects/halogenos/files/$device/${version}.zip/download</DirectUrl>"
-  echo "  <HttpUrl>http://halogenos.org/upload/ROM/7/?dir=$device</HttpUrl>"
-  echo "  <Android>$android</Android>"
-  echo "  <CheckMD5>"$(md5sum $OUT/$version.zip | awk '{print $1}')"</CheckMD5>"
-  echo "  <FileSize type=\"integer\">"$(stat --printf="%s" $OUT/${version}.zip)"</FileSize>"
-  echo "  <Developer>$MAINTAINER</Developer>"
-  echo "  <WebsiteURL nil=\"true\">halogenOS.org</WebsiteURL>"
-  echo "  <DonateURL nil=\"true\">halogenOS.org/donate</DonateURL>"
-  echo "  <Changelog>$CHANGELOG</Changelog>"
-  echo "</ROM>"
-}
-
-mkdir -p changelogs
-touch changelogs/${version}.txt
-changelog_path="$changelog_path"
-if [ -z "$changelog_path" ]; then
-  changelog_path=changelogs/${version}.txt
-  if [ "$DO_NOT_NANO" != "true" ]; then
-    nano changelogs/${version}.txt
-  fi
-fi
-
-CHANGELOG="$(cat $changelog_path)"
-
-if [ -z "$MISTER_MAINTAINER" ]; then
-  echo "Tell me your name, mister maintainer: "
-  read MISTER_MAINTAINER
-fi
-
-echo "Hi, ${MISTER_MAINTAINER}!"
-
-MAINTAINER="$MISTER_MAINTAINER"
-
-write_xml > $product.xml
-git add -A
-git commit -m "$version"
-;;
+    version="$XOS_VERSION"
+    device=$(echo $version | cut -d _ -f 2)
+    android=$(echo $version | cut -d _ -f 3)
+    product=${version%_*}
+    product=${product%_*}
+    date=$(echo $version | cut -d _ -f 3 | cut -d . -f 1)
+    product=${product/XOS_/}
+    if [ -z "$the_zip" ]; then
+      the_zip="$OUT/$version.zip"
+    fi
+    
+    function write_xml() {
+      echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+      echo "<ROM>"
+      echo "  <RomName>$product</RomName>"
+      echo "  <VersionName><![CDATA[ $version ]]></VersionName>"
+      echo "  <VersionNumber type=\"integer\">"$(date +%Y%m%d)"</VersionNumber>"
+      echo "  <DirectUrl>https://sourceforge.net/projects/halogenos/files/$device/${version}.zip/download</DirectUrl>"
+      echo "  <HttpUrl>http://halogenos.org/upload/ROM/7/?dir=$device</HttpUrl>"
+      echo "  <Android>$android</Android>"
+      echo "  <CheckMD5>"$(md5sum $the_zip | awk '{print $1}')"</CheckMD5>"
+      echo "  <FileSize type=\"integer\">"$(stat --printf="%s" $OUT/${version}.zip)"</FileSize>"
+      echo "  <Developer>$MAINTAINER</Developer>"
+      echo "  <WebsiteURL nil=\"true\">halogenOS.org</WebsiteURL>"
+      echo "  <DonateURL nil=\"true\">halogenOS.org/donate</DonateURL>"
+      echo "  <Changelog>$CHANGELOG</Changelog>"
+      echo "</ROM>"
+    }
+    
+    mkdir -p changelogs
+    touch changelogs/${version}.txt
+    changelog_path="$changelog_path"
+    if [ -z "$changelog_path" ]; then
+      changelog_path=changelogs/${version}.txt
+      if [ "$DO_NOT_NANO" != "true" ]; then
+        nano changelogs/${version}.txt
+      fi
+    fi
+    
+    CHANGELOG="$(cat $changelog_path)"
+    
+    if [ -f $device-maintainer.txt ]; then
+      MISTER_MAINTAINER=$(<$device-maintainer.txt)
+    fi
+    
+    if [ -z "$MISTER_MAINTAINER" ]; then
+      echo "Tell me your name, mister maintainer: "
+      read MISTER_MAINTAINER
+    fi
+    
+    echo "Hi, ${MISTER_MAINTAINER}!"
+    
+    MAINTAINER="$MISTER_MAINTAINER"
+    
+    write_xml > $product.xml
+    git add -A
+    git commit -m "$version"
+  ;;
 esac
