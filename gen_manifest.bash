26dc893ee3e7dea55631977430ebaf6c8d12b228
Simao Gomes Viana <xdevs23@outlook.com>
2018-02-25 21:30:14 +0100

    gen_manifest: 7 -> 8
    Change-Id: Id5379fc30746553cb2f5edda5e554c576dacd4b5

--- gen_manifest.bash
+++ gen_manifest.bash
@@ -25,7 +25,7 @@ case $1 in
       echo "  <VersionName><![CDATA[ $version ]]></VersionName>"
       echo "  <VersionNumber type=\"integer\">"$(date +%Y%m%d)"</VersionNumber>"
       echo "  <DirectUrl>https://sourceforge.net/projects/halogenos/files/$device/${version}.zip/download</DirectUrl>"
-      echo "  <HttpUrl>http://halogenos.org/upload/ROM/7/?dir=$device</HttpUrl>"
+      echo "  <HttpUrl>http://halogenos.org/upload/ROM/8/?dir=$device</HttpUrl>"
       echo "  <Android>$android</Android>"
       echo "  <CheckMD5>"$(md5sum $the_zip | awk '{print $1}')"</CheckMD5>"
       echo "  <FileSize type=\"integer\">"$(stat --printf="%s" $OUT/${version}.zip)"</FileSize>"
